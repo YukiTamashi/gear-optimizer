4728e383f29ff0f6dd7a8e497865a1885022b751
Yukinori <38091245+YukiTamashi@users.noreply.github.com>
2023-05-29 09:38:17 -0300

    figured out how to query

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,75 @@
-use axum::{routing::get, Router};
-use elasticsearch::{http::transport::Transport, Elasticsearch, IndexParts};
+use axum::{http::HeaderMap, routing::get, Router};
+use elasticsearch::{
+    http::{request::JsonBody, transport::Transport, Method},
+    Elasticsearch, SearchParts,
+};
 use serde_json::json;
 
-static XIV_API: &str = "https://xivapi.com";
+static XIV_API: &str = "https://xivapi.com/search";
 
 async fn get_item() -> String {
     let transport = Transport::single_node(XIV_API).unwrap();
+    let body: JsonBody<serde_json::Value> = json!(
+        {
+            "query": {
+              "bool": {
+                "must": [
+                  {
+                    "wildcard": {
+                      "NameCombined_en": "*aiming*"
+                    }
+                  }
+                ],
+                "filter": [
+                  {
+                    "range": {
+                      "ItemSearchCategory.ID": {
+                        "gt": "1"
+                      }
+                    }
+                  },
+                  {
+                    "range": {
+                      "LevelItem": {
+                        "gte": "100"
+                      }
+                    }
+                  },
+                  {
+                    "range": {
+                      "LevelItem": {
+                        "lte": "125"
+                      }
+                    }
+                  }
+                ]
+              }
+            },
+        }
+    )
+    .into();
     let client = Elasticsearch::new(transport);
     let request = client
-        .index(IndexParts::IndexId("item", "1"))
-        .body(json!(
-            {
-                "ID": 1613
-            }
-        ))
-        .send()
-        .await;
-    if let Ok(response) = request {
+        .send(
+            Method::Post,
+            XIV_API,
+            HeaderMap::new(),
+            Option::<&serde_json::Value>::None,
+            Some(body),
+            None,
+        )
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    /*if let Ok(response) = request {
         response.text().await.unwrap()
     } else {
         "request failed".to_owned()
-    }
+    }*/
+    format!("{:?}", request)
+    /* */
 }
 
 #[shuttle_runtime::main]
