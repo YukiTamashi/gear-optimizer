f57e6be80201f67c02df566e1d9027a4f969ddc9
Yukinori <38091245+YukiTamashi@users.noreply.github.com>
2023-05-30 10:17:15 -0300

    client

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use axum::{routing::get, Router};
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get());
+    let router = Router::new().route("/", get(a));
 
     Ok(router.into())
+}
+
+async fn a() -> String {
+    "".to_string()
 }
